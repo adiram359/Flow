04ac22b38476d0c107d63bcbd2e1b7e19960d36c
Aditya Ramaakrushnan <48169134+adiram359@users.noreply.github.com>
2020-05-28 17:24:29 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import GoalBar from "./GoalBar.jsx";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Button, ScrollView } from 'react-native';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [goalList, setGoalList] = useState([])
+
+  const removeGoal = (goalTitle) => {
+    setGoalList(goalList.filter((goal) => goal !== goalTitle))
+
+  }
+
   const goalInput = (goal) => {
     setEnteredGoal(goal)
   }
+
   const addGoal = () => {
-    console.log(enteredGoal)
-    setEnteredGoal('')
+    setGoalList((curr) => [...curr, enteredGoal])
   }
+
   return (
     <View style = {styles.container}>
       <TextInput
         placeholder="Type in Your Goal ..."
-        onChangeText={goalInput}
         style={styles.goalinput}
+        onChangeText={goalInput}
         value = {enteredGoal}
         />
       <View style={styles.buttonView}>
@@ -26,6 +34,19 @@ export default function App() {
           onPress={addGoal}
         />
       </View>
+
+      <ScrollView>
+        {goalList.map((goal) =>
+          <View key={goal}style={styles.goalContainer}>
+            <Text key={goal}style = {styles.goal}>
+              {goal}
+            </Text>
+              <Button onPress={() => removeGoal(goal)} color="#E74C3C" title= "X"/>
+          </View>
+
+        )}
+      </ScrollView>
+
     </View>
   );
 }
@@ -33,11 +54,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     alignItems:"center",
-    justifyContent:"center",
     height:"100%",
     width:"100%",
   },
   goalinput: {
+    marginTop:130,
     fontSize:25,
     borderWidth:1,
     width:"90%",
@@ -47,9 +68,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#D6EAF8"
   },
   buttonView: {
-    margin: 10,
+    margin: 6,
     backgroundColor: "#58D68D",
     borderWidth: 2,
     borderRadius: 10,
-  }
+  },
+  goal: {
+    margin:3,
+    fontSize: 25,
+  },
+  goalContainer: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    margin:3,
+    backgroundColor: "#F9E79F",
+    borderColor: "#F1C40F",
+    borderWidth:1,
+    borderRadius: 10,
+    width: 300,
+    height: 40,
+  },
 });
